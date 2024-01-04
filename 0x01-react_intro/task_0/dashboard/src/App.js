7d70732e838acf4bf9b0bfc34ddf5d1ab465ae45
Kennedykachi <kennedyonyekachi@gmail.com>
2024-01-04 21:24:15 +0000

    files for react intro successfully created

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,21 +1,25 @@
-import logo from './holberton-logo.jpg';
+import logo from './HS_logo.jpg';
 import './App.css';
 
 function App() {
-	return (
-		<div className='App'>
-			<div className='App-header'>
-				<img src={logo} alt='Holberton' />
-				<h1>School dashboard</h1>
-			</div>
-			<div className='App-body'>
-				<p>Login to access the full dashboard</p>
-			</div>
-			<div className='App-footer'>
-				<p>Copyright 2020 - holberton School</p>
-			</div>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App.H1"> School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>
+          Login to access the full dashboard
+        </p>
+      </body>
+      <footer className="App-footer">
+        <p>
+          Copyright 2020 - holberton School
+        </p>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
--- 0x01-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import './Notifications.css';
-import closeIcon from './close-icon.png';
+
 import { getLatestNotification } from './utils';
+import closeIcon from './Close-icon.png';
 
-const Notifications = () => {
-	return (
-		<div className='Notifications'>
-			<p style={{ display: 'inline', marginRight: '80%' }}>
-				Here is the list of notifications
-			</p>
-			<button
-				aria-label='Close'
-				onClick={console.log('Close button has been clicked')}
-			>
-				<img style={{ display: 'inline' }} src={closeIcon} alt='Close' />
-			</button>
-			<ul>
-				<li data='default'>New course available</li>
-				<li data='urgent'>New resume available</li>
-				<li
-					data='urgent'
-					dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
-				></li>
-			</ul>
-		</div>
-	);
+export const Notifications = () => {
+  return (
+    <div className='Notifications'>
+      <p>Here is the list of notifications</p>
+      <ul>
+        <li data-priority='default'>New course available</li>
+        <li data-priority='urgent'>New resume available</li>
+        <li
+          data-priority='urgent'
+          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+        ></li>
+      </ul>
+      <button
+        className='close-icon'
+        aria-label='Close'
+        onClick={() => console.log('Close button has been clicked')}
+      >
+        <img
+          src={closeIcon}
+          alt='Close'
+          style={{ height: '10px', width: '10px', padding: '10px'}}
+        />
+      </button>
+    </div>
+  );
 };
-
 export default Notifications;
